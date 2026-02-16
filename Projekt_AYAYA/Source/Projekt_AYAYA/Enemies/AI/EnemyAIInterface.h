6d5dbd7375f8b898490f79d843283d1fa96ed637
ViltisHere <viltishere@gmail.com>
2026-02-16 17:44:25 +0100

    deleted earlier version of unreal project

--- Projekt_AYAYA/Source/Projekt_AYAYA/Enemies/AI/EnemyAIInterface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "PatrolRoute.h"
-
-class EnemyAIInterface
-{
-public:
-	virtual TSubclassOf<AActor> GetPatrolRoute();
-};
--- Projekt_AYAYA/Source/Projekt_AYAYA/Enemies/AI/Tasks/MoveAlongPatrolRoute.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "MoveAlongPatrolRoute.h"
-
-EBTNodeResult::Type UBTT_MoveAlongPatrolRoute::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
-{
-	return EBTNodeResult::Type();
-}
